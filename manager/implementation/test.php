104fe2dc5c282afc673fe1eaf583a458d40381a1
rainbowCN <viewsoft@yeah.net>
2014-08-08 17:18:14 +0800

    Adjusted the meta folder.

--- manager/implementation/test.php
+++ manager/implementation/test.php
@@ -1,30 +1,22 @@
 <?php
-
-
-$username = "admin";
-$password = "123456";
-
-$salt = md5($password.md5($username));
-echo $salt;
-$accessToken = md5($username.$password);
-$token = md5($accessToken.$salt);
-
-print_r('salt: '.$salt);
-
-print_r('accessToken: '.$accessToken);
-print_r('token: '.$token);
-
-
-$at = "112106951fc78c8edf934a7717f0841a";
-
-$result = file_get_contents("./static/config_data/token.json");
-if(preg_match('/^\xEF\xBB\xBF/',$result)){
-	$result=substr($result,3);
-}
-$result = json_decode($result);
-foreach($result as $access){
-	if($access->accessToken===$at){
-		return $access;
-		break;
+function sourceData($path, $suffix) {
+	$handle = opendir($path);
+	$itemArray = array();
+	while (false !== ($file = readdir($handle))) {
+		if (($file == '.') || ($file == '..')) {
+		} elseif (is_dir($path . $file)) {
+			try {
+				$dirtmparr = sourceData($path . $file . '/', $suffix);
+			} catch (Exception $e) {
+				$dirtmparr = null;
+			};
+			array_push($itemArray, array('name'=>$file, 'children'=>$dirtmparr));
+		} else {
+			$_name = basename($file, $suffix);
+			array_push($itemArray, array('name'=>$_name, 'path'=>$path.$_name));
+		}
 	}
+	return $itemArray;
 }
+ $temp = sourceData("test/", ".txt");
+ print_r($temp);
